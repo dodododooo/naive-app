026d62381de17911e5485fd57fe9202abd684cad
gongzheng <meme_456@163.com>
2021-09-29 17:37:10 +0800

    use pinia

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import { store, key } from './store';
+import { createPinia } from 'pinia';
 import plugins from './plugins';
 import naive from 'naive-ui'
 import './style/index.scss';
 
 const app = createApp(App);
-app.use(naive).use(router).use(store, key).use(plugins).mount('#app');
+app.use(naive).use(router).use(createPinia()).use(plugins).mount('#app');
