ff845f2c0f9b1ce2a52334270330f0fb61b62071
gongzheng <meme_456@163.com>
2021-10-25 17:46:30 +0800

    add content component

---
+++ src/components/Content.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useMessage, useNotification } from 'naive-ui';
+
+// 此组件的目的为挂载以下两个全局方法
+window.$message = useMessage();
+window.$notice = useNotification();
+</script>
+
+<template>
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
+</template>
